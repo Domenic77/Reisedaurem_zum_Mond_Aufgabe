5400815e3a80efb22c79e7a2a5c5b907eddbf0cb
Domenic <domenic.troxler@gmx.ch>
2023-12-13 13:51:30 +0100

    Projektdateien hinzufügen.

---
+++ Reisedaurem_zum_Mond_Aufgabe/Mondreise.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace Reisedauer_zum_Mond_Aufgabe
+{
+    public class Mondreise
+    {
+        double speedKmh;
+
+        public Mondreise(double speed)
+        {
+            speedKmh = speed;
+        }
+
+        public double BerechnenTage()
+        {
+            double result = 384400 / speedKmh / 24;
+            return result;
+        }
+
+        public double BerechnenStunden()
+        {
+            double result = 384400 / speedKmh;
+            return result;
+        }
+    }
+}
